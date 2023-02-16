7a466ec96de2ec20aa402de6f4beab6eb4378a2d
SNH-48 <37898949+SNH-48@users.noreply.github.com>
2023-02-16 09:19:38 +0800

    feat: add hid request

--- popup_script.js
+++ popup_script.js
@@ -3,6 +3,9 @@ console.log('Extension popup script (popup_script.js)');
 window.onload = async e => {
   const openTabButtonElement = document.createElement('button');
   openTabButtonElement.appendChild(document.createTextNode('Extension Test Page'));
-  openTabButtonElement.onclick = e => { window.open('index.html', '_blank'); };
+  openTabButtonElement.onclick = e => { 
+    // window.open('index.html', '_blank'); 
+    navigator.hid.requestDevice({filters: []});
+  };
   document.body.appendChild(openTabButtonElement);
 };
